e85719ce836355fd2cddf3e2142184f6069e1b3d
ohidul alam <mdtasib442@gmali.com>
2021-02-26 23:05:49 +0600

    project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './header.css';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="Logo" />
+
+            <nav>
+                <ul>
+                    <li><a href="/shop">Shop</a></li>
+                    <li><a href="/review">Order Review</a></li>
+                    <li><a href="/manage">Manage Inventory here</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
