84e21a221eeca12e3faa26039c24086cb50c97c9
ohidul alam <mdtasib442@gmali.com>
2021-03-04 19:48:06 +0600

    s

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 
 const Shop = () => {
     const product10 = fakeData.slice(0, 10);
@@ -12,6 +13,10 @@ const Shop = () => {
     const handleAddProduct = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length;
+
+        addToDatabaseCart(product.key, count);
     }
 
 
@@ -19,7 +24,8 @@ const Shop = () => {
         <div className='shop-container'>
             <div className="item-container">
                 {
-                    products.map(singleProduct => <Product
+                    products.map((singleProduct, index) => <Product
+                        key={index}
                         handleAddProduct={handleAddProduct}
                         product={singleProduct}
                         showAddToCart={true}
@@ -29,7 +35,7 @@ const Shop = () => {
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
             </div>
-        </div>
+        </div >
     );
 };
 
