8c198b4718a533c21118724f85b15c593ceaebac
Mohammad Tasib <68366378+MdTasib@users.noreply.github.com>
2021-03-22 17:40:26 +0600

    project done
    project done

---
+++ App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/OrderReview/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotMatch from './components/NotMatch/NotMatch';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+import Shipment from './components/Shipment/Shipment';
+import Login from './components/Login/Login';
+import { createContext, useState } from 'react';
+import PrivateRouter from './components/PrivateRouter/PrivateRouter';
+
+export const UserContext = createContext();
+
+function App(props) {
+  const [loggedInUser, setLoggedInUser] = useState({});
+  return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path='/shop'> <Shop></Shop> </Route>
+          <Route path='/review'><Review /></Route>
+          {/* <Route path='/inventory'><Inventory /></Route> */}
+          <PrivateRouter path='/inventory'><Inventory /></PrivateRouter>
+          <PrivateRouter path='/shipment'><Shipment /></PrivateRouter>
+          <Route path='/login'><Login /></Route>
+          <Route exact path='/'><Shop /></Route>
+          <Route path='/product/:productKey'><ProductDetail /></Route>
+          <Route path='/*'><NotMatch /></Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
